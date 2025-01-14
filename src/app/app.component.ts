69d63c50034995285ad9ebc97f25c222d58aac98
Markis94 <134191455+Markis94@users.noreply.github.com>
2025-01-14 09:10:40 +0300

    save sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfigurationDialogComponent } from './dialog/configuration-dialog/configuration-dialog.component';
@@ -13,19 +14,32 @@ import { AppSignalrService } from './services/app-signalr.service';
 export class AppComponent implements OnInit {
   title = 'WebEmailSendlerClient';
   private destroyRef = inject(DestroyRef);
+  protected readonly isMobile = signal(true);
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
   
-  constructor(private signalRService: AppSignalrService, private dialog: MatDialog) {}
+  constructor(
+    private signalRService: AppSignalrService,
+    private dialog: MatDialog
+  ) {
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () =>
+      this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
   ngOnInit() {
     this.signalRService.startConnection();
   }
-  
+
   openConfig() {
     this.dialog
       .open(ConfigurationDialogComponent)
       .afterClosed()
       .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(() => {
-        
-      });
+      .subscribe(() => {});
   }
 }
--- src/app/shared/material.ts
+++ src/app/shared/material.ts
@@ -5,17 +5,22 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatDividerModule } from "@angular/material/divider";
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from '@angular/material/list';
 import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSort, MatSortModule } from "@angular/material/sort";
 import { MatTableModule } from "@angular/material/table";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatTimepickerModule } from '@angular/material/timepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from "@angular/material/tooltip";
+
 const modules = [
   ScrollingModule,
   MatPaginator,
@@ -35,11 +40,16 @@ const modules = [
   MatTabsModule,
   MatCheckboxModule,
   MatTooltipModule,
-  MatSlideToggleModule
+  MatSlideToggleModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+  MatExpansionModule
 ];
 
 @NgModule({
   imports: modules,
   exports: modules,
 })
+
 export class MaterialModule {}
